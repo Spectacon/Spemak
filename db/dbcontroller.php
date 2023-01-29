58e8addc685d735e630fe67e0416921a5da940aa
Spectacon <personalmail3305@gmail.com>
2023-01-29 14:30:04 +0100

    ddcontroller

--- db/dbcontroller.php
+++ db/dbcontroller.php
@@ -4,19 +4,19 @@ class DBController {
 	private $user = "root";
 	private $password = "";
 	private $database = "Spemak";
-	private $conn;
+	private $con;
 	
 	function __construct() {
 		$this->conn = $this->connectDB();
 	}
 	
 	function connectDB() {
-		$conn = mysqli_connect($this->host,$this->user,$this->password,$this->database);
-		return $conn;
+		$con = mysqli_connect($this->host,$this->user,$this->password,$this->database);
+		return $con;
 	}
 	
 	function runQuery($query) {
-		$result = mysqli_query($this->conn,$query);
+		$result = mysqli_query($this->con,$query);
 		while($row=mysqli_fetch_assoc($result)) {
 			$resultset[] = $row;
 		}		
@@ -25,7 +25,7 @@ class DBController {
 	}
 	
 	function numRows($query) {
-		$result  = mysqli_query($this->conn,$query);
+		$result  = mysqli_query($this->con,$query);
 		$rowcount = mysqli_num_rows($result);
 		return $rowcount;	
 	}
